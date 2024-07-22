a7e85da857a2d95acca97c0a2365862805648407
TahsinGokceer <tgokceer@gmail.com>
2024-07-22 21:37:11 +0300

    npm i connect-mongo

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors'); // Güvenlik için
 const session = require('express-session');
-var MongoDBStore = require('connect-mongodb-session')(session);
+// var MongoDBStore = require('connect-mongodb-session')(session);
+const MongoStore = require('connect-mongo');
 const bodyParser = require('body-parser');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -17,10 +18,10 @@ const GameModel = require("./model/gameModel")
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server)
-var store = new MongoDBStore({
-    uri: 'mongodb+srv://tgokceer:X3V35570@cluster0.plwkuyo.mongodb.net/sos',
-    collection: 'users'
-  });
+// var store = new MongoDBStore({
+//     uri: 'mongodb+srv://tgokceer:X3V35570@cluster0.plwkuyo.mongodb.net/sos',
+//     collection: 'users'
+//   });
 
 app.use(cors({
     origin: 'http://localhost:3000', // İzin verilen kaynak (client) adresi
@@ -31,7 +32,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
-app.use(session({ secret: 'my_secret', cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }, store: store, resave: true, saveUninitialized: true}));
+app.use(session({ secret: 'my_secret', cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 }, store: MongoStore.create({ mongoUrl: `${process.env.MONGO_URI}` }), resave: true, saveUninitialized: true}));
 
 // Routes
 app.use("/user", userRoutes)

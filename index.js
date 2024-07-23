7c74190e0c2fda58a7987da313d69eaa3ee5b9c3
TahsinGokceer <tgokceer@gmail.com>
2024-07-23 10:39:26 +0300

    update session cookie

--- index.js
+++ index.js
@@ -36,11 +36,11 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ 
     secret: 'my_secret', 
-    // cookie: { 
-    //     httpOnly: true,
-    //     secure: true,
-    //     sameSite: 'none' }, 
-    //     domain: "http://localhost:3000/",
+    cookie: { 
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none' }, 
+        // domain: "http://localhost:3000/",
     store: MongoStore.create({ mongoUrl: "mongodb+srv://tgokceer:X3V35570@cluster0.plwkuyo.mongodb.net/sos"}) , 
     resave: true, 
     saveUninitialized: true
